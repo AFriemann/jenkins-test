d91bbda929de68727ee6afaa75cde33a6b18f276
Aljosha Friemann <aljosha.friemann@gmail.com>
2021-09-16 13:22:28 +0200

    initial commit

---
+++ libraries/ruby/steps/build.groovy
@@ -0,0 +1,5 @@
+void call(){
+    stage("Ruby: Build"){
+        println "static code analysis from the ruby library"
+    }
+}
---
+++ libraries/ruby/steps/static_code_analysis.groovy
@@ -0,0 +1,5 @@
+void call(){
+    stage("Ruby: Static Code Analysis"){
+        println "build from the ruby library"
+    }
+}
